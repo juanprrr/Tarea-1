20b1e7113f03e6470cb4f8c34275c81d30aea616
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-09-20 18:32:15 -0600

    [Agregada] Clase ServerWorker
    Esta clase hereda Thread de manera que se recorre cada hilo del Servidor mostrando el mensaje de que hay una nueva conexión cada vez que se ejecuta la clase Cliente desde la terminal

--- src/cr/ac/itcr/Cliente/Cliente.java
+++ src/cr/ac/itcr/Cliente/Cliente.java
@@ -1,3 +1,5 @@
+package cr.ac.itcr.Cliente;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
@@ -16,9 +18,8 @@ public class Cliente {
 
             out.writeUTF("Cliente a servidor: hola ");
             String mensaje = in.readUTF();
-
             System.out.println(mensaje);
-            socketCliente.close();
+            //socketCliente.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
